6e6ebcbf0c751a236a02486ae21a94f26674b31c
ArtemKlym <artemklymenko892@gmail.com>
2022-10-20 08:59:58 +0300

    Add menu method

--- src/File_Write.java
+++ src/File_Write.java
@@ -1,7 +1,32 @@
+import java.util.Scanner;
+
 public class File_Write {
     public void file_menu(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What type of file do you want to read"+
+                "\n1)String\t2)Byte\t3)Exit");
 
-        System.out.println("What");
+        int choose = scanner.nextInt();
 
+        switch(choose){
+            case 1: write_string();
+                Main.menu();
+                break;
+
+            case 2: write_byte();
+                Main.menu();
+                break;
+
+            default: break;
+        }
+
+        scanner.close();
+    }
+
+    private void write_byte() {
+    }
+
+    private void write_string() {
+        
     }
 }
